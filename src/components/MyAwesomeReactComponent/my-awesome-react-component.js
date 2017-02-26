8a65a73e6d328d971022f918238a23eba4caab60
Denis Coric <dcoric@groundlink.com>
2017-02-26 02:43:08 +0100

    Working on themes

--- src/components/MyAwesomeReactComponent/my-awesome-react-component.js
+++ src/components/MyAwesomeReactComponent/my-awesome-react-component.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {RaisedButton, DatePicker, TextField, TimePicker} from 'material-ui';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import _ from 'lodash';
-
 import avatar from '../../images/react-logo.png';
 import '../../styles/core.scss';
 
 class MyAwesomeReactComponent extends Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
-      textInput1: 'This is empty text',
+      textInput1: '',
       textInput2: '',
       pickupDate: new Date(),
       maxDate: new Date()
     };
+
+    this.handleChangeTime.bind(this);
+    this.handleChangeDate.bind(this);
   }
 
-  componentWillMount() {
+  componentWillMount () {
     var maxDate = new Date();
     maxDate.setFullYear(maxDate.getFullYear() + 2);
     this.setState({maxDate: maxDate});
@@ -32,8 +34,9 @@ class MyAwesomeReactComponent extends Component {
     minutes = _.round(minutes / 5) * 5;
     pickupDate.setHours(date.getHours());
     pickupDate.setMinutes(minutes);
-    console.log(pickupDate, minutes);
+    console.log('Pickup date from state before setting', this.state.pickupDate);
     this.setState({pickupDate: pickupDate});
+    console.log('Pickup date from state after setting', this.state.pickupDate);
   };
 
   handleChangeDate = (event, date) => {
@@ -49,61 +52,62 @@ class MyAwesomeReactComponent extends Component {
   }
 
   render () {
+    const styles = {
+      card: {
+        width: 310,
+        margin: 10,
+        padding: 10
+      }
+    };
     const textColor = this.props.muiTheme.palette.textColor;
     return (
-      <div className="col-lg-4">
-        <Card>
-          <CardHeader
-            title='Example form'
-            subtitle='With date picker'
-            avatar={avatar} >
-            <CardText>
-              <span style={{color: textColor}}>
-                Hello World! {this.state.maxDate.getFullYear()}
-              </span>
-            </CardText>
-            <div className='row justify-content-center'>
-              <TextField floatingLabelText='Enter some text'
-                value={this.state.textInput1}
-                onChange={(event) => { this.setState({textInput1: event.target.value}); }}
-              />
-            </div>
-            <div className='row justify-content-center'>
-              <TextField
-                floatingLabelText='Enter some more text'
-                value={this.state.textInput2}
-                onChange={(event) => { this.setState({textInput2: event.target.value}); }}
-              />
-            </div>
-            <div className='row justify-content-center'>
-              <DatePicker
-                firstDayOfWeek={0}
-                formatDate={(date) => { return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()); }}
-                minDate={new Date()}
-                maxDate={this.state.maxDate}
-                floatingLabelText='Pickup Date'
-                value={this.state.pickupDate}
-                onChange={this.handleChangeDate}
-                autoOk
-              />
-            </div>
-            <div className='row justify-content-center'>
-              <TimePicker
-                floatingLabelText='Pickup Time'
-                autoOk
-                value={this.state.pickupDate}
-                onChange={this.handleChangeTime}
-                pedantic
-              />
-            </div>
-          </CardHeader>
-          <CardActions>
-            <RaisedButton label="Action 1" />
-            <RaisedButton label="Action 2" />
-            <RaisedButton label="Action 3" />
-          </CardActions>
-        </Card>
-      </div>
+      <Card style={styles.card}>
+        <CardHeader
+          title='Example form'
+          subtitle='With date picker'
+          avatar={avatar}>
+          <CardText>
+            <span style={{color: textColor}}>
+                Welcome
+            </span>
+          </CardText>
+          <TextField floatingLabelText='Enter some text'
+            value={this.state.textInput1}
+            onChange={(event) => {
+              this.setState({textInput1: event.target.value});
+            }}
+          /><br />
+          <TextField
+            floatingLabelText='Enter some more text'
+            value={this.state.textInput2}
+            onChange={(event) => {
+              this.setState({textInput2: event.target.value});
+            }}
+          />
+          <DatePicker
+            firstDayOfWeek={0}
+            formatDate={(date) => {
+              return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+            }}
+            minDate={new Date()}
+            maxDate={this.state.maxDate}
+            floatingLabelText='Pickup Date'
+            value={this.state.pickupDate}
+            onChange={this.handleChangeDate}
+            autoOk
+          />
+
+          <TimePicker
+            floatingLabelText='Pickup Time'
+            value={this.state.pickupDate}
+            onChange={this.handleChangeTime}
+            pedantic
+          />
+        </CardHeader>
+        <CardActions>
+          <RaisedButton label='Confirm action' primary fullWidth />
+        </CardActions>
+      </Card>
     );
   }
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Welcome.scss';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from '../MyAwesomeReactComponent';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+import {
+  blue500, blue700,
+  deepOrangeA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack
+} from 'material-ui/styles/colors';
+
 export default class App extends Component {
-  render() {
+
+  render () {
+    const muiTheme = getMuiTheme({
+      palette: {
+        primary1Color: blue500,
+        primary2Color: blue700,
+        primary3Color: grey400,
+        accent1Color: deepOrangeA200,
+        accent2Color: grey100,
+        accent3Color: grey500,
+        textColor: darkBlack,
+        alternateTextColor: white,
+        canvasColor: white,
+        borderColor: grey300,
+        pickerHeaderColor: blue500,
+        shadowColor: fullBlack
+      }
+    });
     return (
-      <div className="container">
-        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-          <MyAwesomeReactComponent />
-        </MuiThemeProvider>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <MyAwesomeReactComponent />
+      </MuiThemeProvider>
     );
   }
 }
